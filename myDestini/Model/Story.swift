628cc5d1b5eb75a1c3fb812d6dfc00e82f44a16e
Dmitrii <dmitrii.nazarov7703@gmail.com>
2023-04-29 13:33:38 +0300

    Initial commit

---
+++ myDestini/Model/Story.swift
@@ -0,0 +1,16 @@
+//
+//  Story.swift
+//  myDestini
+//
+//  Created by Dmitrii Nazarov on 17.04.2023.
+//
+
+import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
